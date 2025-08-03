8fa3af5bb2ebf07eb6f970c8e2d5447ffb848a7c
Rob Woods <78261399+rob-woods@users.noreply.github.com>
2025-08-03 14:03:37 +0100

    added initial javascript for convert and render

--- index.js
+++ index.js
@@ -6,7 +6,40 @@
 
 const inputField = document.getElementById("unit-num")
 const convertBtn = document.getElementById("convert-btn")
+const lengthOutput = document.getElementById("length-output")
+const metreToFeet = 3.281
+const litreToGallon = 0.264
+const kiloToPound = 2.204
+
+let unit = 20
+let metresOutput = 0
+let feetOutput = 0
+let litresOutput = 0
+let gallonsOutput = 0
+let kilosOutput = 0
+let poundsOutput = 0
 
 convertBtn.addEventListener("click", function() {
-  console.log("unit")
+  unit = inputField.value
+  convert(unit)
 })
+
+function metricToImperial(inputValue, conversionRatio) {
+  return (inputValue * conversionRatio).toFixed(3)
+}
+
+function imperialToMetric(inputValue, conversionRatio) {
+  return (inputValue / conversionRatio).toFixed(3)
+}
+
+function convert(inputValue) {
+  feetOutput = metricToImperial(inputValue, metreToFeet)
+  console.log(feetOutput)
+  metresOutput = imperialToMetric(inputValue, metreToFeet)
+  console.log(metresOutput)
+  render()
+}
+
+function render() {
+  lengthOutput.innerHTML = `${unit} metres = ${feetOutput} feet | ${unit} feet = ${metresOutput} metres`
+}
